eeec44a0ffc14a8450bb01b0e3b2c2d839594d78
tokaretin <tokaretin@gmail.com>
2023-05-23 17:33:04 +0200

    Добавьте файлы проекта.

---
+++ SimpleCode_cycle_FOR_debugging/SimpleCode_cycle_FOR_debugging.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	char simbol;
+	int height; // высота
+	int weidth;	// ширина
+
+	std::cout << "символ: ";
+	std::cin >> simbol;
+	std::cout << "высота: ";
+	std::cin >> height;
+	std::cout << "ширина: ";
+	std::cin >> weidth;
+
+	for (int i = 0; i < height; i++)
+	{
+		std::cout << simbol;
+
+		for (int j = 0; j < weidth; j++)
+		{
+			std::cout << simbol;
+		}
+
+		std::cout << '\n';
+
+	}
+}
